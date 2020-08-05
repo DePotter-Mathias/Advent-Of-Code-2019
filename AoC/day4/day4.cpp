9369fa37d6435a5aa945aa6cacd464c91791f024
Mathias De Potter <mathias.depotter@sigames.com>
2020-08-05 12:30:16 +0100

    day 4 small improvement

--- AoC/day4/day4.cpp
+++ AoC/day4/day4.cpp
@@ -2,14 +2,14 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
-#include<string>  
+#include <string>  
 
 using namespace std;
 
 auto check_valid_password1 = []( const string& input )->bool
 {
 	return
-		ceil( log10( stoi( input ) ) ) == 6 &&
+		input.length() == 6 &&
 		is_sorted( input.cbegin(), input.cend() ) &&
 		adjacent_find( input.cbegin(), input.cend() ) != input.cend();
 };
@@ -18,24 +18,21 @@ auto check_valid_password2 = []( const string& input )->bool
 {
 
 	bool correct_double = false;
-	const auto length = input.length();
-	for ( short i = 0; i < length; ++i )
+	const size_t length = input.length();
+	for ( size_t i = 0; i < length - 1 && !correct_double; ++i )
 	{
-		short count = 0;
-		for ( auto j = i + 1; j < length && input[i] == input[j++]; )
-		{
-			++count;
-		}
-		if ( count == 1 )
-		{
-			correct_double = true;
-			break;
-		}
-		i += count;
+		//check if next one is the same
+		bool next_same = input[i] == input[i + 1];
+		//make sure next one after that one isn't the same; so the chain is only 2 long. if outside the array just return true
+		bool next_but_one_same = (i + 2 < length) ? input[i] != input[i + 2] : true;
+		//check previous one if not same; indicating i is the start of this chain. again, outside = ok
+		bool previous_not_same = (i > 0) ? input[i] != input[i - 1] : true;
+
+		correct_double = next_same && previous_not_same && next_but_one_same;
 	}
 
 	return
-		ceil( log10( stoi( input ) ) ) == 6 &&
+		input.length() == 6 &&
 		is_sorted( input.cbegin(), input.cend() ) &&
 		correct_double;
 };
@@ -43,21 +40,16 @@ auto check_valid_password2 = []( const string& input )->bool
 
 int main()
 {
-	const int
+	constexpr unsigned int
 		puzzle_input_min = 183564u,
 		puzzle_input_max = 657474u;
 
 	unsigned int count_part1 = 0, count_part2 = 0;
-	for ( int possible_password = puzzle_input_min; possible_password < puzzle_input_max; ++possible_password )
+	for ( unsigned int possible_password = puzzle_input_min; possible_password < puzzle_input_max; ++possible_password )
 	{
-		if ( check_valid_password1( to_string( possible_password ) ) )
-		{
-			++count_part1;
-		}
-		if ( check_valid_password2( to_string( possible_password ) ) )
-		{
-			++count_part2;
-		}
+		//increment if true; branchless
+		count_part1 += check_valid_password1( to_string( possible_password ) );
+		count_part2 += check_valid_password2( to_string( possible_password ) );
 	}
 
 	cout << "part 1 result: " << count_part1 << " number of possible passwords." << endl;
