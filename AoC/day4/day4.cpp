5c93d7e95e40470689d78ae0163e533e3e5e9fcb
Mathias De Potter <mathias.depotter@sigames.com>
2020-08-06 21:01:25 +0100

    day4 improvements

--- AoC/day4/day4.cpp
+++ AoC/day4/day4.cpp
@@ -6,17 +6,36 @@
 
 using namespace std;
 
-auto check_valid_password1 = []( const string& input )->bool
+bool check_valid_password1( const string& input )
 {
-	return
-		input.length() == 6 &&
-		is_sorted( input.cbegin(), input.cend() ) &&
-		adjacent_find( input.cbegin(), input.cend() ) != input.cend();
+	const auto length = input.length();
+
+	if ( length != 6 )
+	{
+		return false;
+	}
+
+	bool sorted = true;
+	bool has_adjes = false;
+
+	for ( size_t i = 0; i < length - 1; i++ )
+	{
+		sorted &= input[i] <= input[i + 1];
+		has_adjes |= input[i] == input[i + 1];
+	}
+
+	return sorted && has_adjes;
+
+	//algorithm alternative
+	//return
+	//	input.length() == 6 &&
+	//	adjacent_find( input.cbegin(), input.cend() ) != input.cend() &&
+	//	is_sorted( input.cbegin(), input.cend() );
 };
 
-auto check_valid_password2 = []( const string& input )->bool
+//assumes to be ran only if check_valid_password1 return true on the same input
+bool check_valid_password2( const string& input )
 {
-
 	bool correct_double = false;
 	const size_t length = input.length();
 	for ( size_t i = 0; i < length - 1 && !correct_double; ++i )
@@ -31,25 +50,32 @@ auto check_valid_password2 = []( const string& input )->bool
 		correct_double = next_same && previous_not_same && next_but_one_same;
 	}
 
-	return
-		input.length() == 6 &&
-		is_sorted( input.cbegin(), input.cend() ) &&
-		correct_double;
+	return correct_double;
 };
-
 
 int main()
 {
-	constexpr unsigned int
-		puzzle_input_min = 183564u,
-		puzzle_input_max = 657474u;
+	constexpr unsigned int	puzzle_input_min = 183564u;
+	constexpr unsigned int	puzzle_input_max = 657474u;
 
-	unsigned int count_part1 = 0, count_part2 = 0;
+	unsigned int count_part1 = 0;
+	unsigned int count_part2 = 0;
+
+	string input_string;
+
 	for ( unsigned int possible_password = puzzle_input_min; possible_password < puzzle_input_max; ++possible_password )
 	{
-		//increment if true; branchless
-		count_part1 += check_valid_password1( to_string( possible_password ) );
-		count_part2 += check_valid_password2( to_string( possible_password ) );
+		input_string = to_string( possible_password );
+		if ( check_valid_password1( input_string ) )
+		{
+			++count_part1;
+
+			//assumes to be ran only if check_valid_password1 return true on the same input
+			if ( check_valid_password2( input_string ) )
+			{
+				++count_part2;
+			}
+		}
 	}
 
 	cout << "part 1 result: " << count_part1 << " number of possible passwords." << endl;
